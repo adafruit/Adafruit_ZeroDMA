99421bdb413159686eb2fdd98cc509c0b204b699
dean <deanm1278@gmail.com>
2017-12-04 17:42:08 -0500

    DM:add support for offsets

--- Adafruit_ZeroDMA.h
+++ Adafruit_ZeroDMA.h
@@ -41,7 +41,9 @@ class Adafruit_ZeroDMA {
   // DMA descriptor functions
   DmacDescriptor *addDescriptor(void *src, void *dst, uint32_t count = 0,
                     dma_beat_size size = DMA_BEAT_SIZE_BYTE,
-                    bool srcInc = true, bool dstInc = true);
+                    bool srcInc = true, bool dstInc = true, 
+					uint32_t stepSize = DMA_ADDRESS_INCREMENT_STEP_SIZE_1, 
+					bool stepSel = DMA_STEPSEL_DST);
   void            changeDescriptor(DmacDescriptor *d, void *src = NULL,
                     void *dst = NULL, uint32_t count = 0);
 
